aada9a86da3758ac2cc38314bf5d7e439404b1ba
Kashinath Arora <kashinatharora29@gmail.com>
2020-09-05 16:31:24 +0530

    making it better

--- server.ts
+++ server.ts
@@ -45,7 +45,7 @@ app.get('*', (req, res) => {
 });
 
 app.get('/*', function(req,res) {  
-  res.sendFile(path.join(__dirname+'/dist/index.html'));   
+  res.sendFile(path.join(__dirname+'/src/index.html'));   
 });
 
 // Start up the Node server
