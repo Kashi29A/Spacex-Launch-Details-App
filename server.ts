32cf3a8e3efb551c504c68ffbe841f792d53799a
Kashinath Arora <kashinatharora29@gmail.com>
2020-09-05 15:14:18 +0530

    deploying on heroku

--- server.ts
+++ server.ts
@@ -46,5 +46,5 @@ app.get('*', (req, res) => {
 
 // Start up the Node server
 app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
+  console.log(`Spacex Launch Details app started on --> http://localhost:${PORT}`);
 });
